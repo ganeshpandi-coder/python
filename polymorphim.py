ec1247354c6de09a646cdbb981703c2e43e7530d
ganeshpandi-coder <ganesh936319@gmail.com>
2026-03-13 19:52:15 +0530

    Add files via upload

---
+++ polymorphim.py
@@ -0,0 +1,10 @@
+class animal:
+    def sound(self):
+        print("animal make a sound")
+class dog (animal):
+    def sound(self):
+        print("dog barks")
+obj=dog()
+obj.sound()
+            
+
